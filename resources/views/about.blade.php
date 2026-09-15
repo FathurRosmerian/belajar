e70afed99923b49b47fab6243f42690f078cfd41
Fathur <fathur197808@gmail.com>
2026-09-15 17:17:39 +0700

    Belajar#1

---
+++ resources/views/about.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About Us</title>
+    <link rel="stylesheet" href="css/style1.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@400;500;600;700&display=swap" rel="stylesheet">
+</head>
+<body>
+
+    
+    <nav>
+
+        <img id="Logo" src="img/Logo.jpg" alt="Logo">
+
+        <div class="navbar" id="#">
+        <a href="#"><i>About Us</i></a>
+        <a href=""><i>Catalogue</i></a>
+        <a href=""><i>PriceList</i></a>
+        <a href=""><i>Contact Us</i></a>
+        </div>
+
+    </nav>
+    
+    <div class="card">
+        <video id="Main" autoplay muted loop playsinline>
+            <source src="img/Teaser.mp4" type="video/mp4">
+        </video>
+        <h1>Masih teringat jelas saat pertama kali kami menyalakan oven dan membuka pintu toko ini pada 30 April 2020. Di tengah situasi yang tak mudah saat itu, senyuman dan dukungan kalian menjadi alasan utama kami terus mengaduk adonan, menghias kue, dan menyebarkan rasa manis setiap harinya.Namun, perjalanan tidak selalu mulus. Ada saatnya kami harus berhenti sejenak, merapikan kembali mimpi, dan beristirahat. Perjalanan yang sempat terhenti selama empat tahun ini tentu membawa rindu yang amat dalam—rindu akan aroma mentega yang memenuh ruangan, rindu riuhnya pesanan di hari spesial, dan terutama, rindu menyapa kalian semua. Hari ini, rasa rindu itu akhirnya terobati. Dengan resep autentik yang tetap hangat, kualitas bahan yang makin disempurnakan, dan semangat baru yang lebih besar, kami resmi kembali buka! Terima kasih untuk kalian yang tak pernah lupa dan setia menunggu. Oven kami sudah kembali menyala, dan kami siap menjadi bagian dari momen manis dalam hidupmu lagi. ✨🧁</h1>
+    </div>
+
+</body>
+</html>
